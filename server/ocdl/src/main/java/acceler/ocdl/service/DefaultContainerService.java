0b9f5ce65497e52b555561350e86630ff777a2fa
WBQ1995 <boqianwang1995@gmail.com>
2019-02-07 11:12:58 -0500

    MongoDB first try

--- server/ocdl/src/main/java/acceler/ocdl/service/DefaultContainerService.java
+++ server/ocdl/src/main/java/acceler/ocdl/service/DefaultContainerService.java
@@ -11,6 +11,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.bson.Document;
+import com.mongodb.client.*;
+import com.mongodb.MongoClient;
+
 @Service
 public class DefaultContainerService implements ContainerService {
     private static final Map<User, Integer> assignedContainers = new ConcurrentHashMap<>();
@@ -79,6 +83,38 @@ public class DefaultContainerService implements ContainerService {
             }
         }
 
+        //just try mongoDB
+
+        try{
+            // 连接到 mongodb 服务
+            MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+            // 连接到数据库
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("Oneclick");
+            System.out.println("Connect to database successfully");
+
+
+            MongoCollection<Document> collection = mongoDatabase.getCollection("templates");
+            System.out.println("集合 templates 选择成功");
+
+            //检索所有文档
+            /**
+             * 1. 获取迭代器FindIterable<Document>
+             * 2. 获取游标MongoCursor<Document>
+             * 3. 通过游标遍历检索出的文档集合
+             * */
+            FindIterable<Document> findIterable = collection.find();
+            MongoCursor<Document> mongoCursor = findIterable.iterator();
+            while(mongoCursor.hasNext()){
+                System.out.println(mongoCursor.next());
+            }
+
+
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+
+        
         return availables;
     }
 
