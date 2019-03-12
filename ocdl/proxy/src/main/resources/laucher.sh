6232afb6fd1745fc3104ecb3ba6d8ac1305a1782
IvyLingZhijing <ivyling063@gmail.com>
2019-03-12 13:58:19 -0400

    fix bug in laucher.sh.

--- ocdl/proxy/src/main/resources/laucher.sh
+++ ocdl/proxy/src/main/resources/laucher.sh
@@ -2,18 +2,17 @@
 
 sudo service httpd start
 
-new_file_name = $1
-echo $new_file_name
+echo $1
 
 cd /home/git
-mkdir $new_file_name
+mkdir $1
 
-cd $new_file_name
+cd $1
 git init --bare --shared
 git config --file config http.receivepack true
 
 cd hooks
 sudo mv post-update.sample post-update
 sudo sed -i '$d' post-update
-sudo bash -c "echo curl http://54.89.140.122:8081/job/$new_file_name/build?token=ivy >> post-update"
+sudo bash -c "echo curl http://54.89.140.122:8081/job/$1/build?token=ivy >> post-update"
 
