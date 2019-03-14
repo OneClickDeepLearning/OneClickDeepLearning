9b3a8b4ff7e66560fa0021924d6cdbdea67f94b7
Your Name <315214192@qqq.com>
2019-03-14 16:35:20 -0400

    auth start cpu container

--- ocdl/cx/src/test/java/acceler.ocdl/service/impl/DefaultModelServiceTest.java
+++ ocdl/cx/src/test/java/acceler.ocdl/service/impl/DefaultModelServiceTest.java
@@ -1,6 +1,7 @@
 package acceler.ocdl.service.impl;
 
 import acceler.ocdl.model.User;
+import acceler.ocdl.service.KubernetesService;
 import acceler.ocdl.service.ModelService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import static org.junit.Assert.*;
 
 public class DefaultModelServiceTest {
 
+    @Autowired
+    private ModelService service;
+
     @Test
     public void copyModels() {
-        DefaultModelService service = new DefaultModelService();
         User user1 = new User();
         user1.setProjectId(1L);
         user1.setUserId(1001L);
