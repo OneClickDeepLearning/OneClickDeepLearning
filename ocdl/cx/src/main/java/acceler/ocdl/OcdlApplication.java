02f90680dff9e28d8cc4634bb48bb97894aaa122
Tann-chen <tianen.chen@outlook.com>
2019-03-16 16:56:24 -0400

    add manager auth functions

--- ocdl/cx/src/main/java/acceler/ocdl/OcdlApplication.java
+++ ocdl/cx/src/main/java/acceler/ocdl/OcdlApplication.java
@@ -2,12 +2,19 @@ package acceler.ocdl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PreDestroy;
 
 
 @SpringBootApplication
 public class OcdlApplication {
     public static void main(String[] args){
         SpringApplication.run(OcdlApplication.class, args);
+    }
+
+
+    @PreDestroy
+    public void preDestory() {
+        System.out.println("exit.....");
     }
 }
