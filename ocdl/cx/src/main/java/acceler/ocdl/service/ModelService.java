c3759e48d5811cf8f27552a7ef0835fe89b405d9
IvyLingZhijing <ivyling063@gmail.com>
2019-04-16 21:38:04 -0400

    Refactory: add java doc in service interface.

--- ocdl/cx/src/main/java/acceler/ocdl/service/ModelService.java
+++ ocdl/cx/src/main/java/acceler/ocdl/service/ModelService.java
@@ -10,21 +10,59 @@ public interface ModelService {
 
     /**
      * move the model from userspace to stage space, name is formatted at the same time
-     * @param user
+     * the status of moved model is new, and the format of the fileName = "modelName + timestamp" + "suffix"
+     * @param user used for get userspace path, userspacePath = "HDFS path" + "projectName-userID"
      */
     void initModelToStage(User user);
 
-
+    /**
+     * approval model
+     * move model file from folder "new"  to folder "approval" and update the model object
+     * the format of the fileName = "modelName" + "timestamp" + "algorithm" + "version" + "suffix"
+     * the format of version = "v*.*"
+     * @param model model that need to approve
+     * @param algorithmName the name of algorithm that chose when approve
+     * @param version the version that chose when approve
+     */
     void approvalModel(Model model, String algorithmName, Algorithm.UpgradeVersion version);
 
+    /**
+     * reject model
+     * move model file from folder "new"  to folder "reject" and update the model object
+     * he format of the fileName = "modelName" + "timestamp" + "suffix"
+     * @param model model that need to reject
+     */
     void rejectModel(Model model);
 
+    /**
+     * reject model
+     * move model file from folder "approval" or "reject"  to folder "new" and update the model object
+     * the format of the fileName = "modelName" + "timestamp"
+     * @param model model that need to undo
+     */
     void undo(Model model);
 
+    /**
+     * push model to git repository
+     * copy model file from folder "approval" to git repository, and git push
+     * @param source the path of the model that need to push to git
+     * @param newModelName = "algorithm" + "version" + "suffix" (the format of version = "v*.*")
+     */
     void pushModeltoGit(String source, String newModelName);
 
+    /**
+     * get models by status
+     * @param status specific status
+     * @return list of Model
+     */
     List<Model> getModelsByStatus(Model.Status status);
 
+    /**
+     * verify is the file of model is exist in status folder
+     * @param modelName the name of the model
+     * @param status the status of the model
+     * @return true exist the model file; else not exist.
+     */
     boolean existModel(String modelName, Model.Status status);
 
 
--- ocdl/cx/src/main/java/acceler/ocdl/service/ProjectService.java
+++ ocdl/cx/src/main/java/acceler/ocdl/service/ProjectService.java
@@ -4,7 +4,16 @@ import acceler.ocdl.model.Project;
 
 public interface ProjectService {
 
+    /**
+     * update project configuration
+     * @param updatedProjectInfo the Project obj that contains update info
+     * @return updatedProject obj
+     */
     Project updateProjectConfiguration(Project updatedProjectInfo);
 
+    /**
+     * get Project obj that contains project configuration info
+     * @return Project obj
+     */
     Project getProjectConfiguration();
 }
