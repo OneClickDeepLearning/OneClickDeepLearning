80bd33ead1245f4d15d2b276cba37106ed20ca71
IvyLingZhijing <ivyling063@gmail.com>
2019-04-22 01:27:50 -0400

    Add pwd output in terminal.

--- ocdl/ls/src/main/java/com/ocdl/client/service/impl/DefaultSegmentService.java
+++ ocdl/ls/src/main/java/com/ocdl/client/service/impl/DefaultSegmentService.java
@@ -12,12 +12,17 @@ import java.nio.file.Paths;
 @Service
 public class DefaultSegmentService implements SegmentService {
 
-    private final String MODELBASEPATH = "/models/lesion_segmentation";
-    private final String PICBASEPATH = "/pictures";
-    private final String SEGPICBASEPATH = "/pictures_segmentation";
+    private final String MODELBASEPATH = "models/lesion_segmentation";
+    private final String PICBASEPATH = "pictures";
+    private final String SEGPICBASEPATH = "pictures_segmentation";
 
 
     public File run(String pictureName) {
+
+        String currentPath = CmdHelper.runCommand("pwd");
+        System.out.println("The currentPath is: ");
+        System.out.println(currentPath);
+
 
         StringBuilder cmd = new StringBuilder();
         cmd.append("python3 ");
@@ -33,8 +38,8 @@ public class DefaultSegmentService implements SegmentService {
         cmd.append(Paths.get(SEGPICBASEPATH, outputPictureName));
 
         String result = CmdHelper.runCommand(cmd.toString());
+        System.out.println("If has result:" + !result.isEmpty());
         System.out.println("The result is: ");
-        System.out.println(result.isEmpty());
         System.out.println(result);
 
         return new File(Paths.get(SEGPICBASEPATH, outputPictureName).toString());
