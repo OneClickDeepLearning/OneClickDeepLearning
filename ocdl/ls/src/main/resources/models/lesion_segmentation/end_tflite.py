319086a82396d6a014e8f387bf418bfe4bb02f92
IvyLingZhijing <ivyling063@gmail.com>
2019-04-27 14:22:08 -0400

    Fixbug-5.

--- ocdl/ls/src/main/resources/models/lesion_segmentation/end_tflite.py
+++ ocdl/ls/src/main/resources/models/lesion_segmentation/end_tflite.py
@@ -17,10 +17,10 @@ def jaccard_distance(y_true, y_pred, smooth=1):
 
 def getMemCpu():
     info = psutil.virtual_memory()
-    print u'Memory use：',psutil.Process(os.getpid()).memory_info().rss
-    print u'Total memory：',info.total
-    print u'Memory percentage：',info.percent
-    print u'Num of cpu：',psutil.cpu_count()
+    print "Memory use：", psutil.Process(os.getpid()).memory_info().rss
+    print "Total memory：",info.total
+    print "Memory percentage：",info.percent
+    print "Num of cpu：",psutil.cpu_count()
 
 
 model_path = sys.argv[1]
