951d7f8f503ca7be148319abc452057f80f21dc1
Tann-chen <tianen.chen@outlook.com>
2019-05-04 18:04:24 -0400

    refecotry-build-finish

--- ocdl/cx/src/main/java/acceler/ocdl/service/impl/DefaultHdfsService.java
+++ ocdl/cx/src/main/java/acceler/ocdl/service/impl/DefaultHdfsService.java
@@ -39,14 +39,13 @@ public class DefaultHdfsService implements HdfsService {
      * the request is redirected to a datanode where the file data is to be written
      * step two:  Submit another HTTP PUT request using the URL in the Location header with the file data to be written
      * this function returns the URL in the location header
+     *
      * @param fileName the file name to be uploaded to hdfs
      * @return url of the datanode where the file data is to be written
      * @throws HdfsException
      */
-    public String uploadFile(String fileName) throws HdfsException{
-
-        String result ="http://3.92.26.165:50075/webhdfs/v1/CommonSpace/" + fileName + "?op=CREATE&user.name=hadoop&namenoderpcaddress=hadoop-master:9000&createflag=&createparent=true&overwrite=true";
+    public String uploadFile(String fileName) throws HdfsException {
+        String result = "http://3.92.26.165:50075/webhdfs/v1/CommonSpace/" + fileName + "?op=CREATE&user.name=hadoop&namenoderpcaddress=hadoop-master:9000&createflag=&createparent=true&overwrite=true";
         return result;
-
     }
 }
