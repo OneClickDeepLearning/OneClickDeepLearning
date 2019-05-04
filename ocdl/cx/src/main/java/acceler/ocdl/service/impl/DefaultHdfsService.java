eef231d8caa2072681e9062dd335b498602621a3
BoyuHuo <boyu.huo.china@gmail.com>
2019-05-04 17:56:59 -0400

    HDFS recovery

--- ocdl/cx/src/main/java/acceler/ocdl/service/impl/DefaultHdfsService.java
+++ ocdl/cx/src/main/java/acceler/ocdl/service/impl/DefaultHdfsService.java
@@ -33,4 +33,21 @@ public class DefaultHdfsService implements HdfsService {
             throw new HdfsException(e.getMessage());
         }
     }
+
+    /**
+     * WebHDFS Create and Write to a File needs two steps
+     * step one: Submit a HTTP PUT request without automatically following redirects and without sending the file data
+     * the request is redirected to a datanode where the file data is to be written
+     * step two:  Submit another HTTP PUT request using the URL in the Location header with the file data to be written
+     * this function returns the URL in the location header
+     * @param fileName the file name to be uploaded to hdfs
+     * @return url of the datanode where the file data is to be written
+     * @throws HdfsException
+     */
+    public String uploadFile(String fileName) throws HdfsException{
+
+        String result ="http://3.92.26.165:50075/webhdfs/v1/CommonSpace/" + fileName + "?op=CREATE&user.name=hadoop&namenoderpcaddress=hadoop-master:9000&createflag=&createparent=true&overwrite=true";
+        return result;
+
+    }
 }
