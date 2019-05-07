5a2d20f1cc7f324b7ba2cb0cc5018bc51fd74343
IvyLingZhijing <ivyling063@gmail.com>
2019-05-06 22:26:12 -0400

    Temporarily deprecated the ConfigurationService.

--- ocdl/ls/src/main/java/com/ocdl/client/service/impl/DefaultConfigurationService.java
+++ ocdl/ls/src/main/java/com/ocdl/client/service/impl/DefaultConfigurationService.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Deprecated
 @Service
 public class DefaultConfigurationService implements ConfigurationService {
 
