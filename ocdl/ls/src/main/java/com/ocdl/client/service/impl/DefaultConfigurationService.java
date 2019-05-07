223ca59475391058e2f397dfc506139e432b7cb8
IvyLingZhijing <ivyling063@gmail.com>
2019-05-06 22:30:06 -0400

    Temporarily deprecated the ConfigurationController.

--- ocdl/ls/src/main/java/com/ocdl/client/service/impl/DefaultConfigurationService.java
+++ ocdl/ls/src/main/java/com/ocdl/client/service/impl/DefaultConfigurationService.java
@@ -7,7 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Deprecated
 @Service
 public class DefaultConfigurationService implements ConfigurationService {
 
