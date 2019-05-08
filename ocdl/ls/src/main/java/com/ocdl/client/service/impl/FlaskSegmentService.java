4ac053c5f794b0449b62471fbf016a7db117f9f4
IvyLingZhijing <ivyling063@gmail.com>
2019-05-07 23:12:32 -0400

    Change to Apache HttpClient.

--- ocdl/ls/src/main/java/com/ocdl/client/service/impl/FlaskSegmentService.java
+++ ocdl/ls/src/main/java/com/ocdl/client/service/impl/FlaskSegmentService.java
@@ -1,16 +1,26 @@
 package com.ocdl.client.service.impl;
 
 import com.ocdl.client.service.SegmentService;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -47,20 +57,55 @@ public class FlaskSegmentService implements SegmentService {
         String outputPath = Paths.get(WORKSPACEPATH,SEGPICBASEPATH, basePictureName + "_seg.png").toString();
         String groundTruthPath = Paths.get(WORKSPACEPATH,GROUNDTRUTHBASEPATH, basePictureName + "_segmentation.png").toString();
 
-        LinkedMultiValueMap body = new LinkedMultiValueMap();
-        body.add("model_path", modelPath);
-        body.add("test_pic_path", picturePath);
-        body.add("output_image_path", outputPath);
-        body.add("ground_truth_path", groundTruthPath);
+//        LinkedMultiValueMap body = new LinkedMultiValueMap();
+//        body.add("model_path", modelPath);
+//        body.add("test_pic_path", picturePath);
+//        body.add("output_image_path", outputPath);
+//        body.add("ground_truth_path", groundTruthPath);
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+//        HttpEntity entity = new HttpEntity(body, headers);
+//
+//        ResponseEntity<String> responds = restTemplate.exchange(FLASKSEVERURL, HttpMethod.POST, entity, String.class);
+//        System.out.println("============================================");
+//        System.out.println(responds.getBody());
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        HttpEntity entity = new HttpEntity(body, headers);
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(FLASKSEVERURL);
 
-        ResponseEntity<String> responds = restTemplate.exchange(FLASKSEVERURL, HttpMethod.POST, entity, String.class);
-        System.out.println("============================================");
-        System.out.println(responds.getBody());
+            // add header
+            post.setHeader("Content-type", "application/json; charset=utf-8");
+
+            List<NameValuePair> urlParameters = new ArrayList<>();
+            urlParameters.add(new BasicNameValuePair("model_path", modelPath));
+            urlParameters.add(new BasicNameValuePair("test_pic_path", picturePath));
+            urlParameters.add(new BasicNameValuePair("output_image_path", outputPath));
+            urlParameters.add(new BasicNameValuePair("ground_truth_path", groundTruthPath));
+
+            post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+            HttpResponse response = client.execute(post);
+            System.out.println("\nSending 'POST' request to URL : " + FLASKSEVERURL);
+            System.out.println("Post parameters : " + post.getEntity());
+            System.out.println("Response Code : " +
+                    response.getStatusLine().getStatusCode());
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            System.out.println(result.toString());
+        }catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
 
         return new File(Paths.get(SEGPICBASEPATH, basePictureName + "_seg.png").toString());
     }
