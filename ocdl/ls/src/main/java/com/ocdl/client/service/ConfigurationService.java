47849b97720d9051ff8f01431e0b35af90d675cf
BoyuHuo <boyu.huo.china@gmail.com>
2019-05-15 07:38:52 -0400

    linux path fixed

--- ocdl/ls/src/main/java/com/ocdl/client/service/ConfigurationService.java
+++ ocdl/ls/src/main/java/com/ocdl/client/service/ConfigurationService.java
@@ -1,7 +1,5 @@
 package com.ocdl.client.service;
 
-import java.io.IOException;
-
 public interface ConfigurationService {
     String getProjectName();
     Boolean setProjectName(String projectName);
