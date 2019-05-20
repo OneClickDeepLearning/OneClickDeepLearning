9d8cf9aeb61e494698d87391fe70f59c2674e7c3
IvyLingZhijing <ivyling063@gmail.com>
2019-05-20 15:46:43 -0400

    Fix bug.

--- ocdl/cx/src/main/java/acceler/ocdl/model/ApprovedModel.java
+++ ocdl/cx/src/main/java/acceler/ocdl/model/ApprovedModel.java
@@ -30,12 +30,11 @@ public class ApprovedModel extends Model implements Cloneable {
 
     public NewModel convertToNewModel() {
         NewModel newModel = new NewModel();
-        System.out.println("convert to new model" + this.modelId);
         newModel.setModelId(this.modelId);
         newModel.setName(this.name);
         newModel.setCommitTime(TimeUtil.currentTime());
 
-        return new NewModel();
+        return newModel;
     }
 
     public Long getReleasedVersion() {
