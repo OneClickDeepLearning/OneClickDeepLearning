2f47c61547f5eab856ce7d7f41c24ab0506f0152
IvyLingZhijing <ivyling063@gmail.com>
2019-07-13 16:04:37 -0400

    Fix bug in initStorage.

--- ocdl/cx/src/main/java/acceler/ocdl/controller/PersistenceController.java
+++ ocdl/cx/src/main/java/acceler/ocdl/controller/PersistenceController.java
@@ -5,7 +5,6 @@ import acceler.ocdl.model.StorageLoader;
 import acceler.ocdl.utils.SerializationUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
