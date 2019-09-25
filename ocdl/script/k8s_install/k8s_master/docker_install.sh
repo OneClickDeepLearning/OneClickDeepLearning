589c3d743e11b7c0d0a80fe5c8ed524a0e562b60
WBQ1995 <boqianwang1995@gmail.com>
2019-09-25 14:50:54 -0400

    Kubernetes installation scripts added

---
+++ ocdl/script/k8s_install/k8s_master/docker_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#UNINSTALL OLD VERSIONS
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+#SET UP THE REPOSITORY
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+#INSTALL DOCKER ENGINE - COMMUNITY
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+#INSTALL DOCKER-COMPOSE
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
