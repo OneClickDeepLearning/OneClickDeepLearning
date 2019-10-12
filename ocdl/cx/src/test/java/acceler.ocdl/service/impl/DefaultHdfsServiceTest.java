e3896e16a0afaba74ecdda3d775a8d541a9cb7ef
boqian <boqianwang1995@gmail.com>
2019-10-11 20:41:47 -0400

    add list files and download file api, undone.

--- ocdl/cx/src/test/java/acceler.ocdl/service/impl/DefaultHdfsServiceTest.java
+++ ocdl/cx/src/test/java/acceler.ocdl/service/impl/DefaultHdfsServiceTest.java
@@ -1,15 +1,21 @@
 package acceler.ocdl.service.impl;
 
+import acceler.ocdl.exception.HdfsException;
 import acceler.ocdl.service.HdfsService;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import static org.junit.Assert.*;
 
 public class DefaultHdfsServiceTest {
-
-    @Autowired HdfsService hdfsService;
 
     @Test
     public void uploadFile() {
@@ -20,8 +26,29 @@ public class DefaultHdfsServiceTest {
 
     @Test
     public void listFiles() {
-        Path path = new Path("/");
-        hdfsService.listFiles(path);
+        listFiles(new Path("/"));
+    }
+
+    public void listFiles(Path path) {
+        Configuration conf = new Configuration();
+        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+        try {
+            URI uri = new URI("hdfs://ec2-34-230-14-57.compute-1.amazonaws.com:9000");
+            //returns the configured filesystem implementation.
+            FileSystem hdfs = FileSystem.get(uri, conf, "hadoop");
+            FileStatus[] files = hdfs.listStatus(new Path("/"));
+            for (FileStatus file: files) {
+                if(file.isDirectory()) {
+                    System.out.println(">>>" + file.getPath());
+                    listFiles(file.getPath());
+                } else {
+                    System.out.println(file.getPath());
+                }
+            }
+
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            throw new HdfsException(e.getMessage());
+        }
     }
 
 }
