9b43019e96d8ce3ae2bc456e4ddedda384d19048
IvyLingZhijing <ivyling063@gmail.com>
2020-03-22 14:07:55 -0400

    Fix bug in KafkaService.

--- ocdl/cx/src/main/java/acceler/ocdl/service/impl/KafkaService.java
+++ ocdl/cx/src/main/java/acceler/ocdl/service/impl/KafkaService.java
@@ -1,11 +1,15 @@
 package acceler.ocdl.service.impl;
 
+import acceler.ocdl.dao.PlatformMetaDao;
+import acceler.ocdl.entity.PlatformMeta;
+import acceler.ocdl.exception.OcdlException;
 import acceler.ocdl.service.MessageQueueService;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.Properties;
@@ -15,17 +19,19 @@ public class KafkaService implements MessageQueueService {
 
     private static final Logger log = Logger.getLogger(KafkaService.class);
 
+    @Autowired
+    private PlatformMetaDao platformMetaDao;
+
     private KafkaProducer<String, String> producer;
-
-    @Value("${KAFKA.KAFKA_URL}")
-    public static String kafkaUrl;
-
 
     public void createProducer() {
 
         if (producer == null) {
             // create kafka producer
             Properties props = new Properties();
+            PlatformMeta meta = platformMetaDao.findById(1L)
+                    .orElseThrow(() -> new OcdlException("Fail to get Platform meta data."));
+            String kafkaUrl = meta.getKafkaUrl();
             props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl);
             props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
             props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -42,10 +48,9 @@ public class KafkaService implements MessageQueueService {
 
     @Override
     public void send(String topic, String data) {
-        if (producer == null) {
-            createProducer();
-            System.out.println("create producer");
-        }
+
+        createProducer();
+        System.out.println("create producer");
 
         try {
             producer.send(new ProducerRecord<>(topic, data));
