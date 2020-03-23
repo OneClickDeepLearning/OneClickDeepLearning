50a15a6b8a8e14302a37784a2c5d10f42216d8be
IvyLingZhijing <ivyling063@gmail.com>
2020-03-23 19:37:43 -0400

    Fix bug in signup.

--- ocdl/cx/src/main/java/acceler/ocdl/utils/GlobalExceptionHandler.java
+++ ocdl/cx/src/main/java/acceler/ocdl/utils/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ import acceler.ocdl.dto.Response;
 import acceler.ocdl.exception.OcdlException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = OcdlException.class)
+    @ResponseBody
     public Response exceptionsHandling(OcdlException exception) {
         String frontErrorMsg= exception.getMessage();
 
